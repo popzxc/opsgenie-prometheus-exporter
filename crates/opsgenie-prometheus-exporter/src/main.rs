13f7a4eb89275c8e11db61e69d1f7d3afa6bd7d3
Igor Aleksanov <popzxc@yandex.ru>
2024-07-28 15:01:29 +0400

    Add observability stack

--- crates/opsgenie-prometheus-exporter/src/main.rs
+++ crates/opsgenie-prometheus-exporter/src/main.rs
@@ -18,13 +18,23 @@ mod metrics;
 struct Config {
     pub opsgenie_base_url: url::Url,
     pub opsgenie_api_key: String,
+    #[serde(default = "Config::default_prometheus_port")]
     pub prometheus_port: u16,
+    #[serde(default = "Config::default_log_format")]
     pub log_format: String,
     #[serde(default = "Config::default_polling_interval_secs")]
     pub polling_interval_secs: u64,
 }
 
 impl Config {
+    fn default_log_format() -> String {
+        "plain".into()
+    }
+
+    fn default_prometheus_port() -> u16 {
+        3652
+    }
+
     fn default_polling_interval_secs() -> u64 {
         60 * 5
     }
---
+++ etc/README.md
@@ -0,0 +1,8 @@
+# Observability stack for local development
+
+This folder contains files required to run Prometheus & Grafana locally.
+
+Files here are used by the [docker-compose.yml](../docker-compose.yml).
+
+This folder and `docker-compose.yml` are based on the [Dockprom](https://github.com/stefanprodan/dockprom/)
+repository, which is licensed under [MIT license](https://raw.githubusercontent.com/stefanprodan/dockprom/master/LICENSE).
